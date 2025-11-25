737fdadf04e23f5a1111b884cf3355f02ec05577
ITF3RR4R1 <ferrari.traderoficial@gmail.com>
2025-11-25 08:07:41 -0300

    Update version and author information in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="iqoptionapi",
-    version="0.1.0",
+    version="0.2.0",
     description="Wrapper Python para IQ Option API",
-    author="L0k1",
-    author_email="cassioms764@gmail.com",
-    url="https://github.com/cassDS/iqoptionapi",
+    author="",
+    author_email="",
+    url="https://github.com/itmanagersupport/iqoptionapi_",
     packages=find_packages(),
-    install_requires=["pylint", "requests", "websocket-client==1.8.0"],
+    install_requires=["pylint", "requests", "websocket-client"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
